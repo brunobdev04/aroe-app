3e7614448ae1e995e8ac81687fc7ea6875bd9e45
gabriel.old <gabrieljrg2006@gmail.com>
2026-05-20 12:42:10 -0300

    feat: componente nav completo e BrowserRouter configurado

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Hero from './components/Hero/Hero'
+import Navbar from './components/Navbar/Navbar'
 import './App.css'
 
 function App() {
   return (
     <>
+      <Navbar />
       <Hero />
     </>
   )
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,6 +2,8 @@ export default function Button({ children, variant = 'primary', onClick = () => 
     const variants = {
         primary: "bg-indigo-600 hover:bg-indigo-700 text-white rounded-xl shadow-lg shadow-indigo-500/30",
         outline: "bg-white/5 border border-white/30 text-white hover:bg-white/12 rounded-xl backdrop-blur-sm",
+
+        // TODO mudar esses dois aqui pra o estilo do figma dps
         navOutline: "border border-white/60 text-white hover:bg-white/10 rounded-full",
         navPrimary: "bg-purple-700 hover:bg-purple-800 text-white rounded-full",
     }
